205706c62a1dd603b82d7b46b42661730cf3d50c
aqib123-jpg <aqibk5867@gmail.com>
2024-12-17 20:58:54 +0500

    Added a new feature for user authentication

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    // host: 'localhost',
-    // user: 'root',
-    // password: 'MySqL321@', 
-    // database: 'CHECKINGECOMMERCE'
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+
 });
 
 db.connect(err => {
